93dc54a6a9cd95150d6eef4ae48edd7900af84ad
Juliano Seus <JulianoSeusDev@outlook.com>
2021-02-23 10:14:19 -0300

    conexao com o mongo

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 let express = require("express");
 let app = express();
+let mongoose = require("mongoose");
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/guiapics", {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log("Conectado com o banco");
+    }).catch((err) => {
+        console.log(err);
+    });
 
 app.get("/", (req, res) => {
     res.json({});
