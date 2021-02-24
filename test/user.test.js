95e9e7824968766f5ccf7c81dcbe1fc1b993c9a1
Juliano Seus <JulianoSeusDev@outlook.com>
2021-02-24 09:03:04 -0300

    usuario principa

--- test/user.test.js
+++ test/user.test.js
@@ -2,6 +2,24 @@ let app = require("../src/app");
 let supertest = require("supertest");
 let request = supertest(app);
 
+let mainUser = {name: "Tester", email: "tester@tester.com", password: "tester123"}
+
+beforeAll(() => {
+    // Inserir usuário Tester no Banco
+    return request.post("/user")
+    .send(mainUser)
+    .then(res => {})
+    .catch(err => {console.log(err)});
+
+});
+
+afterAll(() => {
+    return request.delete(`/user/${mainUser.email}`)
+    .then(res => {})
+    .catch(err => {console.log(err)});
+
+});
+
 describe("Cadastro de usuário",() => {
 
     test("Deve cadastrar um usuário com sucesso", () => {
